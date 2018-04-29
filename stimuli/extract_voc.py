0e62a16c385b6c2554cd40fdbcf2804fd2ad9707
n_athan <pi12b076@gmail.com>
2018-04-29 03:37:11 +0300

    vocab + word2vec

--- stimuli/extract_voc.py
+++ stimuli/extract_voc.py
@@ -3,32 +3,50 @@ import string
 import re, string
 import numpy as np
 import os
+import pickle
 
-translator = str.maketrans('', '', string.punctuation)
-words = []
-for file in glob.glob('./*[!.py]'):
-    print(file)
+def save_pickle(data,savepath):
+    fdr = '/'.join(savepath.strip().split('/')[:-1])
+    print(fdr)
+    if not os.path.exists(fdr):
+        os.makedirs(fdr)
+    with open(savepath+'.pkl', 'wb') as fn:
+        pickle.dump(data, fn)
+def preprocess_file(filepath):
+    translator = str.maketrans('', '', string.punctuation.replace('-',''))
+    words = []
     with open(file,'r') as fl:
         for raw_line in fl:
             l = raw_line.strip()
             l = (l.translate(translator)).lower().split(' ')
+            print(l)
+            for wd in l:
+                print(wd)
+                if '-' in wd:
+                    l.extend(wd.split('-'))
+                    print(l)
+                    l.remove(wd)
+                    print(l)
             words.extend(l)
+    voc_raw = list(set(words))
+    voc_raw.remove('')
+    vocab = dict()
+    for item in voc_raw:
+        vocab[item] = 1
+    return vocab
 
-voc_raw = list(set(words))
-vocab = dict()
-for item in voc_raw:
-    vocab[item] = 1
+
+
 c=0
-print(vocab)
 
-fileSize = os.path.getsize(fileName)
+fileSize = os.path.getsize('../word_embeddings/glove.42B.300d.txt')
 
 progress = 0
 with open('../word_embeddings/glove.42B.300d.txt', 'r') as embds:
     model = {}
     for id,line in enumerate(embds):
         progress = progress + len(line)
-        progressPercent = (1.0*progress)/fileSize
+        progressPercent = round(100*((1.0*progress)/fileSize),2)
         if id % 10000 == 0:
             print(progressPercent)
         splitLine = line.split()
@@ -37,3 +55,5 @@ with open('../word_embeddings/glove.42B.300d.txt', 'r') as embds:
             embedding = np.array([float(val) for val in splitLine[1:]])
             model[word] = embedding
             c+=1
+            voc_raw.remove(word)
+save_pickle(model,'./word2vec')
