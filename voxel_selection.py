ce831ba7af6066fc61da1bd7572027d9e80e9320
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2018-04-30 18:12:00 +0300

    voxel selection
    Signed-off-by: Giorgos Paraskevopoulos <geopar@central.ntua.gr>

--- voxel_selection.py
+++ voxel_selection.py
@@ -1,15 +1,15 @@
+import errno
 import numpy as np
 import scipy.io as scio
 import sys
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Ridge
-from utils import load_pickle,disc_pr,load_data_meta,extract_sent_embed
+from utils import load_pickle, disc_pr, load_data_meta, extract_sent_embed
 import argparse
 import glob
 import string
-
-
+import os
 
 def load_embeddings(data_file):
     with open(data_file, 'r') as fd:
@@ -32,6 +32,12 @@ def parse_voxels(loaded_mat):
 def parse_meta(loaded_mat):
     voxels2neigh = loaded_mat['meta']['voxelsToNeighbours'][0][0]
     nneigh = loaded_mat['meta']['numberOfNeighbors'][0][0].flatten()
+    return voxels2neigh, nneigh
+
+
+def parse_meta1(meta):
+    voxels2neigh = meta['voxelsToNeighbours'][0][0]
+    nneigh = meta['numberOfNeighbors'][0][0].flatten()
     return voxels2neigh, nneigh
 
 
@@ -71,15 +77,12 @@ def split_folds(X, y, k=10):
     return X_cv_train, y_cv_train, X_cv_test, y_cv_test
 
 
-def main(argv):
-    # TODO change to argparse
-    mat_file = argv[1]
-    glove_file = argv[2]
-    out_file = argv[3]
+def voxel_scores(participant, name, mri_vectors, semantic_vectors, meta):
+    out_file = '{}_{}.npy'.format(participant, name)
     alpha = 1.0
     n_folds = 10
-    mri_vectors, voxels2neigh, nneigh = load_voxels(mat_file)
-    semantic_vectors, semantic_dims = load_embeddings(glove_file)
+    voxels2neigh, nneigh = parse_meta1(meta)
+    semantic_dims = next(iter(semantic_vectors.values())).shape[0]
     num_voxels = voxels2neigh.shape[0]
     num_concepts = len(mri_vectors.keys())
     scores = np.zeros((semantic_dims, num_voxels))
@@ -100,40 +103,35 @@ def main(argv):
             ridge = Ridge(alpha=alpha, normalize=False).fit(X_tr, y_tr)
             y_pred = ridge.predict(X_te)
             y_pred_z = (StandardScaler(with_mean=True, with_std=True)
-                    .fit_transform(y_pred))
+                        .fit_transform(y_pred))
             dot_sum += y_pred_z * y_cv_test[fold]
         scores[:, v] = dot_sum / float(num_concepts)
     np.save(out_file, scores)
 
 
-
-
-
 def load_exp1(data_dir):
-
     main_dir = glob.glob(data_dir + '/*')
     w2vec_dict = load_pickle('./stimuli/word2vec.pkl')
     exp_id = int((data_dir.split('/')[-1]).split('_')[0][-1])
-    assert exp_id==1
+    assert exp_id == 1
     for fld in main_dir:
-    # for every participant
-        data_files = sorted(glob.glob(fld+'/*'))
-        dt_fls_grouped = [tuple(data_files[i:i + 2]) for i in range(0, len(data_files), 2)]
+        # for every participant
+        data_files = sorted(glob.glob(fld + '/*'))
+        dt_fls_grouped = [tuple(data_files[i:i + 2]) for i in
+                          range(0, len(data_files), 2)]
         print(fld)
         disc_pr()
 
         # for every file wordcloud pictures and sentences cases
         for data_group in dt_fls_grouped:
-            data_dict,metadata = load_data_meta(data_group)
+            data_dict, metadata = load_data_meta(data_group)
             word_dict = dict()
-            for word,_ in data_dict.items():
+            for word, _ in data_dict.items():
                 word_dict[word] = w2vec_dict[word]
-            yield data_group[0], data_dict, word_dict, metadata
-
+            yield fld, data_group[0], data_dict, word_dict, metadata
 
 
 def load_exp23(data_dir):
-
     main_dir = glob.glob(data_dir + '/*')
 
     exp_id = int((data_dir.split('/')[-1]).split('_')[0][-1])
@@ -143,35 +141,47 @@ def load_exp23(data_dir):
         # for every participant
 
         data_files = sorted(glob.glob(fld + '/*'))
-        dt_fls_grouped = [tuple(data_files[i:i + 2]) for i in range(0, len(data_files), 2)]
+        dt_fls_grouped = [tuple(data_files[i:i + 2]) for i in
+                          range(0, len(data_files), 2)]
 
         # for every file here data and meta
         for data_group in dt_fls_grouped:
 
-            data_dict,metadata = load_data_meta(data_group)
+            data_dict, metadata = load_data_meta(data_group)
             word_dict = dict()
-            for sent,_ in data_dict.items():
+            for sent, _ in data_dict.items():
                 word_dict[sent] = extract_sent_embed(sent)
-            return data_group[0], data_dict, word_dict, metadata
+            return fld, data_group[0], data_dict, word_dict, metadata
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i','-data_dir',dest="data_dir",required=True)
+    parser.add_argument('-i', '-data_dir', dest="data_dir", required=True)
     args = parser.parse_args()
 
     assert 'data_processed' not in args.data_dir, 'You should rename your data_directory to data_processed'
 
-    exp = int(((args.data_dir).split('/')[-1]).split('_')[0][-1])
-    assert exp ==1 or exp==2 or exp==3
-    assert 'exp' in (args.data_dir).split('/')[-1]
-    if  exp == 1:
+    exp = int((args.data_dir.split('/')[-1]).split('_')[0][-1])
+    assert exp == 1 or exp == 2 or exp == 3
+    assert 'exp' in args.data_dir.split('/')[-1]
+    if exp == 1:
         data_gen = load_exp1(args.data_dir)
         disc_pr()
         # how to access a generator silly boy :*
         for x in data_gen:
             print(x[0])
     elif exp == 2 or exp == 3:
-         load_exp23(args.data_dir)
-    else :
+        load_exp23(args.data_dir)
+    else:
         raise ValueError("Illegal value for data folder .Select from{1,2,3}")
